514e7328001a3bac4055506d34cf7697f5da9bd1
Sergei Malyshka <104466946+SergMalyshka@users.noreply.github.com>
2024-03-27 19:40:14 -0400

    renamed controllers

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 
 const orderRoutes = require('./order-routes.js');
-const menuRoutes = require('./menu-routes.js');
+const dishRoutes = require('./dish-routes.js');
+const loginRoutes = require('./login-routes.js');
+
+
 router.use('/orders', orderRoutes);
 router.use('/menu', menuRoutes);
-const userRoutes = require('./user-routes.js');
-
 router.use('/login', userRoutes)
 module.exports = router;
