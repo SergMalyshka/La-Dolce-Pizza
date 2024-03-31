3685edd3bee0cb5577a5f87131c3add72ea39814
David Carioto <148878612+Carioto@users.noreply.github.com>
2024-03-31 11:31:36 -0400

    update or delete menu item

--- controllers/api/dish-routes.js
+++ controllers/api/dish-routes.js
@@ -1,7 +1,9 @@
 const { Dish } = require('../../models');
 const router = require('express').Router();
 
-// /menu routes
+//      ---  /api/menu routes  ---
+
+// show all dishes
 router.get('/dishes', async (req,res) => {
     try {
         const dbMenu = await Dish.findAll({
@@ -17,11 +19,13 @@ router.get('/dishes', async (req,res) => {
     }
 });
 
+// render add-dish page
 router.get('/dishes/add', async (req,res) => {
     res.render('dish-manager')
     }
 );
 
+// add new dish to database
 router.post('/dishes/add', async (req,res) => {
     try {
         const newDish = await Dish.create({
@@ -35,13 +39,55 @@ router.post('/dishes/add', async (req,res) => {
     }
 });
 
+// render 1 dish to edit or delete
+router.get('/dishes/:id', async (req,res) => {
+    try {
+        const oneDish = await Dish.findOne({
+            where:{ id:req.params.id,
+            },
+            attributes: ['id', 'name', 'price'],
+        })
+        const editDish = oneDish.get({plain:true});
+        console.log(editDish);
+        res.render('dish-edit', {editDish});
+    } catch (err){
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
-
-
-
-
-
-
-
+// edit a menu item
+router.put("/dishes", async (req, res) => {
+    try {
+      const updateDish = await Dish.update(
+        {
+          name: req.body.name,
+          price: req.body.price,
+        },
+        {
+          where: {
+            id: req.body.id,
+          },
+        }
+      );
+      res.status(200).json({ message: "Menu item updated." });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  // delete a menu item
+  router.delete("/dishes/:id", async (req, res) => {
+    try {
+      const delDish = await Dish.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.status(200).json({ message: "Menu item deleted" });
+    } catch (err) {
+      res.status(500).json({ message: "Cannot delete menu item" });
+    }
+  });
 
 module.exports = router;
