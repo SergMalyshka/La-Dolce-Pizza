f643d42f69b9c8b9b7ee5d72eeacd225f3328b92
David Carioto <148878612+Carioto@users.noreply.github.com>
2024-04-01 21:49:36 -0400

    css work

--- seeds/dishSeed.js
+++ seeds/dishSeed.js
@@ -2,18 +2,58 @@ const { Dish } = require('../models');
 
 const dishData = [
     {
-        name: "Cheez Pizza",
+        name: "Pizza - Cheez",
         price: 17.99,
         cost: 4.25,
     },
     {
-        name: "Meat Pizza",
+        name: "Pizza - Meat",
         price: 21.99,
         cost: 5.15,
     },
     {
-        name: "Veggie Pizza",
+        name: "Pizza - Veggie",
         price: 19.99,
+        cost: 4.90,
+    },
+    {
+        name: "Pizza - Buffalo Chicken",
+        price: 20.99,
+        cost: 4.90,
+    },
+    {
+        name: "Pizza - Gyro",
+        price: 19.59,
+        cost: 4.90,
+    },
+    {
+        name: "Fish Sandwich",
+        price: 10.99,
+        cost: 4.90,
+    },
+    {
+        name: "Calzone - Steak",
+        price: 21.99,
+        cost: 4.90,
+    },
+    {
+        name: "Calzone - Chicken",
+        price: 22.99,
+        cost: 4.90,
+    },
+    {
+        name: "Calzone - Italian",
+        price: 19.99,
+        cost: 4.90,
+    },
+    {
+        name: "Wedgie - Gyro",
+        price: 19.19,
+        cost: 4.90,
+    },
+    {
+        name: "Wedgie - Steak",
+        price: 18.99,
         cost: 4.90,
     },
 ];
