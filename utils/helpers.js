671093da717e8ebc1549afa083e6a43f31100956
Serg Malyshka <sergmalyshka@gmail.com>
2024-04-04 18:31:26 -0400

    time change

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
 module.exports = {
     format_time: (date) => {
-      return date.toLocaleTimeString();
+      return date.toLocaleTimeString("en-US");
     },
     format_date: (date) => {
       return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
